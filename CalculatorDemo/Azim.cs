d603f77f57e6856c1956fd277b59cd2d64ca82eb
azim30 <shaikhazima37@gmail.com>
2021-03-23 10:05:59 +0530

    Commit in new class

---
+++ CalculatorDemo/Azim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDemo
+{
+    class Azim
+    {
+        public void Name()
+        {
+            Console.WriteLine("Hello from new class Azim");
+        }
+    }
+}
